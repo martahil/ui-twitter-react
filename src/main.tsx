1af84f162bef75b69d0bfaff509e97e3814de2c0
Marta Hilgert <marta.hil.1988@gmail.com>
2024-08-23 14:52:11 -0300

    Sidebar component

--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+import { Sparkle } from 'phosphor-react'
 
 import './global.css';
 
-import twitterLogo from './assets/logo-twitter.svg'
 import { Tweet } from './components/Tweet';
+import { Sidebar } from './components/Sidebar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='layout'>
-      <aside className='sidebar'>
-        <img className='logo' src={twitterLogo} alt="Logo" />
+      <Sidebar />
 
-        <nav className='main-navigation'>
-          <a className='active' href="">
-            <House weight='fill'/>
-            Home
-          </a>
-          <a href="">
-            <Hash />
-            Explore
-            </a>
-          <a href="">
-          <Bell />
-            Notifications
-          </a>
-          <a href="">
-            <Envelope />
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText />
-            Lists
-          </a>
-          <a href="">
-            <User />
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle />
-            More
-          </a>
-        </nav>
-
-        <button className='new-tweet' type='button'>Tweet</button>
-      </aside>
       <div className='content'>
         <main className='timeline'>
           <div className='timeline-header'>
@@ -59,9 +21,9 @@ createRoot(document.getElementById('root')!).render(
 
           <form className='new-tweet-form'>
             <label htmlFor="tweet">
-              <img src="https://github.com/martahil.png" alt='Marta Hil'/>
-              <textarea  id="tweet" placeholder="What's hapenning?" />
-            </label>  
+              <img src="https://github.com/martahil.png" alt='Marta Hil' />
+              <textarea id="tweet" placeholder="What's hapenning?" />
+            </label>
 
             <button type='submit'>Tweet</button>
           </form>
