b38c42bcf77921b2552ec490125903708a46cfc0
Marta Hilgert <marta.hil.1988@gmail.com>
2024-08-26 18:33:08 -0300

    Form / Saving new tweets

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,26 +1,41 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
-const tweets = [
-  "My first tweet 🙂",
-  "Testing...",
-  "It's working!"
-]
-
 import './Timeline.css';
 
+let newTweet = '';
+
 export function Timeline() {
+
+  const [tweets, setTweets] = useState([
+    "My first tweet 🙂",
+    "Testing...",
+    "It's working!"
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+    setTweets([newTweet, ...tweets ]);
+  }
+
   return (
 
     <main className='timeline'>
 
       <Header title='Home' />          
 
-      <form className='new-tweet-form'>
+      <form onSubmit={createNewTweet} className='new-tweet-form'>
         <label htmlFor="tweet">
           <img src="https://github.com/martahil.png" alt='Marta Hil' />
-          <textarea id="tweet" placeholder="What's hapenning?" />
+          <textarea 
+          id="tweet" 
+          placeholder="What's hapenning?" 
+          onChange={(event) => {
+            newTweet = event.target.value
+          }} 
+          />
         </label>
 
         <button type='submit'>Tweet</button>
